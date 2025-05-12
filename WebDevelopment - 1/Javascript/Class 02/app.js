eb86c9d2a238882a5cead633aa6f9cb745604e64
AdilAhmedShekhani <adilaziz2009@live.com>
2025-05-12 22:43:06 +0500

    Comparision Operators

--- WebDevelopment - 1/Javascript/Class 02/app.js
+++ WebDevelopment - 1/Javascript/Class 02/app.js
@@ -52,3 +52,74 @@
 
 // alert(message);
 
+//  Comparision Operators
+
+// > is greater than
+// < is less than
+// >= is greater than or equal to
+// > is less than or equal to
+
+// var age = 21;
+
+// if (age <= 20) {
+//   console.log("Eligible");
+// }
+
+// if else and else if statements
+
+// var age = 22;
+
+// if (age <= 20) {
+//   console.log("Eligible");
+// } else {
+//   console.log("Not Eligible");
+// }
+
+// var kuchLanahai = prompt("Kia mil raha hai ");
+
+// if (kuchLanahai === "Dahi") {
+//   console.log("le ana");
+// } else if (kuchLanahai === "Dhoodh") {
+//   console.log("Acha Le aao");
+// } else if (kuchLanahai === "Bread") {
+//   console.log("bhai le aao yar");
+// } else {
+//   console.log("Bhai kuch bhi le aao");
+// }
+
+// var language = prompt("Enter Your Language");
+
+// if (language === "javascript") {
+//   var city = prompt("Enter your city");
+//   if (city === "karachi") {
+//     var exp = prompt("Enter your experience");
+//     if (exp === "2 years") {
+//       console.log("Congratulation, you are Hired");
+//     } else {
+//       console.log("Barhe ho jao");
+//     }
+//   } else {
+//     console.log("Karachi Aaja bhai");
+//   }
+// } else {
+//   console.log("JavaScript Seekho Beta");
+// }
+
+//  Falsy Value (6 types) other than this values are true
+
+// "" Empty String ==> False
+// 0 ==> False
+// false ==> False
+// undefind ==> False
+//NaN ==> False
+// null ==> False
+
+// var userName = prompt("Enter your username") || "Not Provided";
+
+// console.log(userName);
+
+// var isAvailable = userName && "yes";
+
+// console.log(isAvailable);
+
+// console.log(userName);
