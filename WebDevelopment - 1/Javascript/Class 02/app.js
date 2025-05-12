3926acd062a410bf2a7519375131bf27223a2297
AdilAhmedShekhani <adilaziz2009@live.com>
2025-05-12 22:41:39 +0500

    Statements (if and Else) && and ||

--- WebDevelopment - 1/Javascript/Class 02/app.js
+++ WebDevelopment - 1/Javascript/Class 02/app.js
@@ -13,3 +13,42 @@
 // var userName = prompt("Please Enter Your User Name");
 
 // console.log(userName);
+
+// == Match condition only
+// === Match condition and type both
+// !== not equal to
+// && and and (will check second condition)
+// || or or (only check one condition)
+
+// if statements (true or false)
+
+// if (true) {
+//   alert("Good");
+// }
+
+// var city = prompt("The city of lights ?");
+// if (city === "karachi") {
+//   alert("Correct");
+// }
+
+// var gender = prompt("Please Enter Your gender");
+// if (gender === "Male") {
+//   alert("Welcome Sir");
+// }
+
+// if (gender === "Female") {
+//   alert("Welcome Maam");
+// }
+
+// var gender = prompt("Please Enter Your gender");
+// var message = "Please Enter Correct one";
+// if (gender === "Male") {
+//   message = "Welcome Sir";
+// }
+
+// if (gender === "Female") {
+//   message = "Welcome Maam";
+// }
+
+// alert(message);
+
