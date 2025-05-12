a51aef098491b451fa4bb6aea03cadf0d7fad0ae
AdilAhmedShekhani <adilaziz2009@live.com>
2025-05-12 22:39:41 +0500

    Prompt

---
+++ WebDevelopment - 1/Javascript/Class 02/app.js
@@ -0,0 +1,15 @@
+// Connect JavaScript
+
+// alert("Hello Students");
+
+//Type of Variable
+
+// var num = true;
+// console.log(typeof num);
+
+// Prompt
+
+// var userName = prompt();
+// var userName = prompt("Please Enter Your User Name");
+
+// console.log(userName);
